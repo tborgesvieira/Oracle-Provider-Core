8814e7d45138b63c059fa6405b2dcf05f6a569d5
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2019-01-19 21:48:31 -0200

    Adicionado o projeto para o github

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly string connectinString = "Data Source=(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.99.100)(PORT = 1521)))(CONNECT_DATA = (SID = xe)));Persist Security Info=True;User ID=Teste;Password=123456;Pooling=True;Connection Timeout=60;";
+
+        static void Main(string[] args)
+        {
+            using (var conn = new OracleConnection(connectinString))
+            {
+                using(var cmd = conn.CreateCommand())
+                {
+                    conn.Open();
+
+                    cmd.CommandText = "select * from pessoa";
+
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("===========================");
+                        Console.WriteLine($"Id:{reader["Id"]}");
+                        Console.WriteLine($"Nome: {reader["Nome"]}");
+                    }
+
+                    Console.WriteLine("===========================");
+
+                    reader.Dispose();                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
